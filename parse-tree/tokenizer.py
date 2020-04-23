0902008866b7ae11b9b4009cf821ba98f2b05abf
Muhammed Eldeeb <meldeeb1998@gmail.com>
2020-04-23 19:27:35 +0200

    create a singleton class 'TokenRepo'

--- parse-tree/tokenizer.py
+++ parse-tree/tokenizer.py
@@ -5,3 +5,46 @@ class Token():
         self.name = name
         self.value = value
 
+class TokenRepo():
+
+    __instance = None
+    __queue = []
+
+    @staticmethod
+    def get_instance():
+        """ static access method """
+        if TokenRepo.__instance == None:
+            TokenRepo()
+        return TokenRepo.__instance
+
+    def __init__(self):
+        """ virtually private constructor  """
+        super().__init__()
+        if TokenRepo.__instance != None:
+            raise Exception("This class is a singleton!")
+        else:
+            TokenRepo.__instance = self
+    def get_queue(self):
+        return TokenRepo.__queue
+
+    def add_token(self, token):
+        TokenRepo.__queue.append(token)
+
+    def has_next(self):
+        pass
+
+    def get_next(self):
+        pass
+
+    def consume(self):
+        pass
+
+t2 = TokenRepo.get_instance()
+print(t2.get_queue())
+t2.add_token(5)
+
+t4 = TokenRepo.get_instance()
+print(t4.get_queue())
+t4.add_token(7)
+t3 = TokenRepo.get_instance()
+print(t3.get_queue())
